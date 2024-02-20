c678ac0b246bab64e615127c5a23377ea3a412ec
unknown <fperaltaperez@gmail.com>
2024-02-20 12:01:57 -0300

    listado de usuarios y creacion de registro, falta multer

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,6 +1,19 @@
 const mainController = require("../controller/mainController")
 const express = require("express")
 const mainRouter = express.Router()
+const multer  = require('multer')
+const path = require("path")
+
+    const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        let folder = path.join(__dirname, '../public/images')
+        cb (null, folder)
+    },
+    filename: function(req, file, cb){
+        let imageName = Date.now() + path.extname(file.originalname)
+        cb(null, imageName )
+    } })
+var fileUpLoad = multer({storage:storage})
 
 mainRouter.get("/", mainController.index)
 
@@ -8,6 +21,7 @@ mainRouter.get("/", mainController.index)
 mainRouter.get("/carrito", mainController.carrito)
 mainRouter.get("/login", mainController.login)
 mainRouter.get("/register", mainController.register)
+mainRouter.post("/register", fileUpLoad.single('avatar'), mainController.register)
 
 
 
